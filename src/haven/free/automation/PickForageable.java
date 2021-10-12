32c5fa34f47fa4b16aa4acd27ace550ebb6a0d31
mervent <lamkaslim@gmail.com>
2021-10-12 22:30:36 +0300

    Add list of custom collectables

--- src/haven/free/automation/PickForageable.java
+++ src/haven/free/automation/PickForageable.java
@@ -4,8 +4,69 @@ import haven.*;
 
 import static haven.OCache.posres;
 
+import java.util.*;
+
 public class PickForageable implements Runnable {
     private GameUI gui;
+    private HashSet<String> collectables = new HashSet<String>(Arrays.asList(
+        "adder",
+        "arrow",
+        "blueberry",
+        "boarspear",
+        "boostspeed",
+        "bram",
+        "cart",
+        "cattail",
+        "cavemoth",
+        "chantrelle",
+        "chick",
+        "chicken",
+        "chives",
+        "clover",
+        "coltsfoot",
+        "crab",
+        "dandelion",
+        "dragonfly",
+        "duskfern",
+        "firefly",
+        "forestlizard",
+        "forestsnail",
+        "frog",
+        "frogspawn",
+        "grasshopper",
+        "grub",
+        "hedgehog",
+        "irrbloss",
+        "jellyfish",
+        "ladybug",
+        "ladysmantle",
+        "lampstalk",
+        "lingon",
+        "mallard",
+        "mistletoe",
+        "mole",
+        "mussels",
+        "opiumdragon",
+        "precioussnowflake",
+        "rabbit",
+        "rat",
+        "rowboat",
+        "rustroot",
+        "sandflea",
+        "silkmoth",
+        "snapdragon",
+        "spindlytaproot",
+        "squirrel",
+        "stingingnettle",
+        "thornythistle",
+        "toad",
+        "wagon",
+        "waterstrider",
+        "wball",
+        "wheelbarrow",
+        "windweed",
+        "yellowfoot"
+    ));
 
     public PickForageable(GameUI gui) {
         this.gui = gui;
@@ -22,7 +83,17 @@ public class PickForageable implements Runnable {
                 } catch (Loading l) {
                 }
                 if (res != null) {
+                    Boolean pick = false;
+                    
                     if (res.name.startsWith("gfx/terobjs/herbs") && !res.name.startsWith("gfx/terobjs/vehicle")) {
+                        pick = true;
+                    }
+
+                    if (this.collectables.contains(res.basename())) {
+                        pick = true;
+                    }
+
+                    if (pick) {
                         double distFromPlayer = gob.rc.dist(gui.map.player().rc);
                         if (distFromPlayer <= 20 * 11
                                 && (herb == null || distFromPlayer < herb.rc.dist(gui.map.player().rc)))
